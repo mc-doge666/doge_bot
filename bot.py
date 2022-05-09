14f06419ca973ee3febb6e71aef68f10b58f236d
mc-doge666 <wangyaocing@outlook.com>
2022-05-09 15:51:53 +0800

    3

--- bot.py
+++ bot.py
@@ -7,4 +7,8 @@ bot = commands.Bot(command_prefix= '/')
 async def on_ready():
     print(">> Bot is online")
 
+@bot.event
+async def on_member_join(member):
+    print(F'{member} join!')
+
 bot.run('OTczMTIxODEzMTUwMzAyMjM4.GWMg48.AQDYsNECs9bGjoxn0P-6JapQHSKfBt-9Odn1Og')
